c57b93f7130c6768a85d35c3a9f08b7f2774f7cd
guru vishwa g <guruvishwa1102@gmail.com>
2026-06-18 18:17:11 +0530

    Fix: Add default export to App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,17 +1,16 @@
-// Example of correct setup
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import Login from './pages/Login';
 import Tracker from './pages/Tracker';
 import Signup from './pages/Signup';
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Login />} />
-        <Route path="/tracker" element={<Tracker />} />
-        <Route path="/signup" element={<Signup />} />
-      </Routes>
-    </BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Login />} />
+      <Route path="/tracker" element={<Tracker />} />
+      <Route path="/signup" element={<Signup />} />
+    </Routes>
   );
-}export default App;
+}
+
+export default App;
